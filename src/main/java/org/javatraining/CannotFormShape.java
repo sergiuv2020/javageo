5d1584a016f88f321cc09f115215c24841930c15
svidrascu <vsergiu@hotmail.com>
2015-05-24 03:41:44 +0300

    forgot exception class dohhhh

---
+++ src/main/java/org/javatraining/CannotFormShape.java
@@ -0,0 +1,11 @@
+package org.javatraining;
+
+/**
+ * Created by Sergiu Vidrascu on 5/23/15.
+ */
+public class CannotFormShape extends Exception {
+
+    public CannotFormShape(String message) {
+        super(message);
+    }
+}
